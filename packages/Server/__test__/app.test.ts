4dcc45528117c9f93afea257bd4874101d9aa80d
Pedro Henrique <77502759+Allchymist@users.noreply.github.com>
2022-02-11 19:39:34 -0300

    mian

--- packages/Server/__test__/app.test.ts
+++ packages/Server/__test__/app.test.ts
@@ -5,21 +5,21 @@ import { validate } from 'uuid';
 // Post /account/register or /account/login
 
 test('Create user', async () => {
-	const response = await request(app).post('/account/register').send({
-		name: 'Teste',
-		email: 'henTiago@gmail.com',
-		password: '123456',
-	});
+  const response = await request(app).post('/account/register').send({
+    name: 'Teste',
+    email: 'henTiago@gmail.com',
+    password: '123456',
+  });
 
-	expect(response.status).toBe(201);
-	expect(validate(response.body.id)).toBe(true);
+  expect(response.status).toBe(201);
+  expect(validate(response.body.id)).toBe(true);
 
-	expect(response.body).toMatchObject({
-		message: 'Usuário cadastrado com sucesso.',
-		name: 'Teste',
-		email: 'henTiago@gmail.com',
-		id: response.body.id,
-		password: '123456',
-		createdAt: new Date().toLocaleDateString('pt-BR'),
-	});
+  expect(response.body).toMatchObject({
+    message: 'Usuário cadastrado com sucesso.',
+    name: 'Teste',
+    email: 'henTiago@gmail.com',
+    id: response.body.id,
+    password: '123456',
+    createdAt: new Date().toLocaleDateString('pt-BR'),
+  });
 });
