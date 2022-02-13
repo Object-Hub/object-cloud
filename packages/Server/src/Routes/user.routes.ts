0bc4ad0560b5b553104941d4e3835f95e3e77438
zThiagoR <thi.magalha13@gmail.com>
2022-02-13 04:07:56 -0300

    Change route to forgot password

--- packages/Server/src/Routes/user.routes.ts
+++ packages/Server/src/Routes/user.routes.ts
@@ -1,11 +1,12 @@
 import { authController } from '../Controllers/User/Auth-controller';
 import { Router } from 'express';
+import { passwordController } from '../Controllers/User/Password-Controller';
 
 const UserRoutes = Router();
 
 UserRoutes.get('/api'); // API User
 UserRoutes.post('/login', authController.login); // Login User
 UserRoutes.post('/register', authController.register); // Register User
-UserRoutes.post('/password'); // Forgot Password User
+UserRoutes.post('/forgot-password', passwordController.forgotPassword); // Forgot Password User
 
 export default UserRoutes;
