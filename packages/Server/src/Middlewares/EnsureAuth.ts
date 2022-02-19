e287bf7605729fb1e763f4228e1dedcc5d066a7d
zThiagoR <thi.magalha13@gmail.com>
2022-02-19 15:06:23 -0300

    The return of middleware after various forms

---
+++ packages/Server/src/Middlewares/EnsureAuth.ts
@@ -0,0 +1,30 @@
+import { NextFunction, Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
+
+interface IPayload {
+  sub: string;
+}
+
+export function EnsureAuth(req: Request, res: Response, next: NextFunction) {
+  const token = req.headers.authorization;
+
+  if (!token) {
+    return res.status(401).send({
+      message: 'Não autorizado.',
+    });
+  }
+
+  const splitToken = token.split(' ')[1];
+
+  try {
+    const { sub } = jwt.verify(splitToken, process.env.SECRET_KEY || 'secret') as IPayload;
+
+    req.body.userId = sub;
+
+    return next();
+  } catch (error) {
+    return res.status(401).json({
+      error: 'Token inválido.',
+    });
+  }
+}
