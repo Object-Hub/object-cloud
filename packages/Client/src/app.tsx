632b335c8b08b9dd511bec99519883c21219b4ff
zThiagoR <thi.magalha13@gmail.com>
2022-02-20 23:55:29 -0300

    Removing routes in file and changing to app.tsx

--- packages/Client/src/app.tsx
+++ packages/Client/src/app.tsx
@@ -1,7 +1,20 @@
-import AppRoutes from "./Routes/routes";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import { DashBoard } from "./pages/private/dashboard";
+import { Home } from "./pages/public/Home";
+import { Login } from "./pages/public/Login";
+import { Register } from "./pages/public/Register";
+
 
 export function App() {
   return (
-      <AppRoutes />
+      <Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/dashboard" element={<DashBoard />} />
+            </Routes>
+        </Router>
   );
 }
