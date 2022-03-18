37cbee680078b86a4dd18147f3d6b65565eade70
Pedro Henrique <pedrohenry015@gmail.com>
2022-03-18 00:45:26 -0300

    edited registerform
    Co-authored-by: zThiagoR <zThiagoR@users.noreply.github.com>

--- Client/src/components/RegisterForm/index.tsx
+++ Client/src/components/RegisterForm/index.tsx
@@ -7,16 +7,17 @@ import styles from './styles.module.scss'
 interface IMessage {
   data?: string;
   type?: string;
+	used?: string;
   warn: boolean;
 }
 
 export function RegisterForm(){
 
-	const [name, setName] = useState('')
-  const [username, setUsername] = useState('')
-	const [email, setEmail] = useState('')
-	const [password, setPassword] = useState('')
-	const [confirmPassword, setConfirmPassword] = useState('')
+	const [name, setName] = useState('');
+  const [username, setUsername] = useState('');
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+	const [confirmPassword, setConfirmPassword] = useState('');
 	const [message, setMessage] = useState({ warn: false } as IMessage);
 
 	const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*\.]).{8,}$/g;
@@ -44,7 +45,7 @@ export function RegisterForm(){
 			return setTimeout(() => setMessage({ warn: false }), 2000);
 		}
 
-		await api.post('/account/register', {
+		api.post('/account/register', {
 			name,
       username,
 			email,
@@ -60,12 +61,11 @@ export function RegisterForm(){
 	return (
 		<div className={styles.Container}>
 			<form onSubmit={handleSubmit} className={styles.Form}>
-				<>{message.warn ?
+				<>{message.warn && message.used == 'name' ?
 					<label> 
 						<div className={
 							message.type === 'sucess' ? styles.Sucess :
-							message.type === 'error' ? styles.Warn :
-							styles.Load
+							message.type === 'error' ? styles.Warn : styles.Load
 						}> {message.data}
 						</div>
 					<br />
@@ -80,7 +80,15 @@ export function RegisterForm(){
 						value={name} 
             onChange={e => {
 							setName(e.target.value);
-						}	
+
+							e.target.value ? setMessage({ warn: false }) :
+							setMessage({
+								data: 'Você não inseriu seu nome.',
+								type: 'error',
+								used: 'name',
+								warn: true
+							});
+						}
 					} />
 				</label>
         <br />
@@ -91,7 +99,18 @@ export function RegisterForm(){
 						type="text" 
 						name="username"
 						value={username} 
-            onChange={e => setUsername(e.target.value)} />
+            onChange={e => {
+							setUsername(e.target.value);
+
+							e.target.value ? setMessage({ warn: false }) :
+							setMessage({
+								data: 'Você não inseriu seu nome de usuário.',
+								type: 'error',
+								used: 'username',
+								warn: true
+							});
+						} 
+					} />
 				</label>
 				<br />
         <label>
@@ -101,7 +120,18 @@ export function RegisterForm(){
 						type="email" 
 						name="e-mail"
 						value={email} 
-            onChange={e => setEmail(e.target.value)}/>
+            onChange={e => {
+							setEmail(e.target.value);
+
+							e.target.value ? setMessage({ warn: false }) :
+							setMessage({
+								data: 'Você precisa digitar seu email.',
+								type: 'error',
+								used: 'email',
+								warn: true
+							});
+						}
+					}/>
 				</label>
 				<br />
 				<label>
@@ -118,27 +148,38 @@ export function RegisterForm(){
 								!regex.test(e.target.value) ? setMessage({
 									data: 'Senha fraca.',
 									type: 'error',
+									used: 'pass',
 									warn: true
 								}) : setMessage({
 									data: 'Senha forte.',
 									type: 'sucess',
+									used: 'pass',
 									warn: true
 								});
-							} else setMessage({ warn: false });
+							} else setMessage({ used: 'pass', warn: false });
 						}
 					}/>
 				</label>
 				<br />
 				<label>
           Confirme sua senha:
-          <input 
+          <input
             placeholder='Digite sua senha novamente'
             type="password" 
             name="confirmPassword" 
             value={confirmPassword} 
-            onChange={e => setConfirmPassword(e.target.value)}/>
-				</label>
+            onChange={e => {
+							setConfirmPassword(e.target.value);
 
+							password !== e.target.value ? setMessage({
+								data: 'As senha não conferem.',
+								type: 'error',
+								used: 'confirmPass',
+								warn: true
+							}) : setMessage({ used: 'pass', warn: false });
+						}
+					}/>
+				</label>
 				<button type="submit">Cadastrar</button>
 				<div className={styles.Register}>
 				Já tem a conta?<a href="/login"> Entrar</a>
